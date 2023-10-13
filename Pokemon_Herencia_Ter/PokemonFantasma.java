08e603851f95748156f9f33190e8c4f2d22bdf62
DannaNegron <134634194+DannaNegron@users.noreply.github.com>
2023-10-13 08:01:44 -0600

    Update PokemonFantasma.java

--- Pokemon_Herencia_Ter/PokemonFantasma.java
+++ Pokemon_Herencia_Ter/PokemonFantasma.java
@@ -11,5 +11,6 @@ public class PokemonFantasma extends Pokemon {
     public PokemonFantasma(String nombre,String tipo, int nivel) {
         super(nombre, "Pokemon Fantasma", nivel, 100);
     }
+}  
 
    
